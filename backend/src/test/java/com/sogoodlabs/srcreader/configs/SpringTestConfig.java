d69810d0e3ca936622f956448c9b1dca83798b49
lxdi <alexfordeal@gmail.com>
2020-05-03 21:19:16 +0300

    postrgres conf

--- backend/src/test/java/com/sogoodlabs/srcreader/configs/SpringTestConfig.java
+++ backend/src/test/java/com/sogoodlabs/srcreader/configs/SpringTestConfig.java
@@ -1,41 +1,43 @@
 package com.sogoodlabs.srcreader.configs;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
 
 
 /**
  * Created by Alexander on 07.04.2018.
  */
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-////@ContextConfiguration(loader=AnnotationConfigContextLoader.class,
-////        classes = {HibernateConfigMain.class, EmbeddedDBConf.class,
-////                FuncDtoMapper.class, FuncDelegate.class})
-//@ContextConfiguration(loader=AnnotationConfigContextLoader.class,
-//        classes = {})
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@ActiveProfiles("test")
 public abstract class SpringTestConfig {
 
-//    static {
-//        SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
-//        builder.bind("java:comp/env/use_database", "false");
-//        try {
-//            builder.activate();
-//        } catch (NamingException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @Autowired
+    EntityManagerFactory etf;
+
+    @Before
+    public void cleanDB(){
+        SessionFactory sessionFactory = etf.unwrap(SessionFactory.class);
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.createQuery("delete from CodeSnippet").executeUpdate();
+        session.createQuery("delete from FuncFlow").executeUpdate();
+        session.createQuery("delete from Func").executeUpdate();
+        session.createQuery("delete from Component").executeUpdate();
+        session.createQuery("delete from Scenario").executeUpdate();
+        session.createQuery("delete from Project").executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+    }
 
 }
--- compile-and-run.py
+++ compile-and-run.py
@@ -8,7 +8,7 @@ for line in process.stdout:
 process.wait()
 
 warPath = os.path.join(os.getcwd(), 'backend', 'build', 'libs', 'ROOT.jar')
-processjava = subprocess.Popen('java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006 -jar %s' % warPath, shell=True)
+processjava = subprocess.Popen('java -Dspring.profiles.active=prod -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006 -jar %s' % warPath, shell=True)
 print(processjava.pid)
 
 command = input('Command: ')
